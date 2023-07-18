ed66ccc5f79b9988c4973a036aa0c94edc84e63c
jagtym <jagtymxd@gmail.com>
2023-07-18 23:38:37 +0200

    add cmp autocompletions

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -44,6 +44,11 @@ require'packer'.startup(function(use)
     use "neovim/nvim-lspconfig"
     use "simrat39/rust-tools.nvim"
     use "mfussenegger/nvim-dap" 
+    use "hrsh7th/cmp-nvim-lsp"
+    use "hrsh7th/cmp-buffer"
+    use "hrsh7th/cmp-path"
+    use "hrsh7th/cmp-cmdline"
+    use "hrsh7th/nvim-cmp"
     use({
     "glepnir/lspsaga.nvim",
     opt = true,
@@ -119,6 +124,39 @@ require'nvim-treesitter.configs'.setup {
   },
 }
 
+local cmp = require'cmp'
+
+cmp.setup({
+    snippet = {
+      -- REQUIRED - you must specify a snippet engine
+      expand = function(args)
+        -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      end,
+    },
+    window = {
+      -- completion = cmp.config.window.bordered(),
+      -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      -- { name = 'luasnip' }, -- For luasnip users.
+      -- { hame = 'ultisnips' }, -- For ultisnips users.
+      -- { name = 'snippy' }, -- For snippy users.
+    }, {
+      { name = 'buffer' },
+    })
+})
+
 local lspconfig = require('lspconfig')
 local util = require('lspconfig/util')
 lspconfig.pyright.setup {}
@@ -161,6 +199,8 @@ rt.setup({
     }
   }
 })
+
+
 
 -- editor config
 --
